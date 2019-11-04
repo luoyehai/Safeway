528483542a1bdadb69cd10a5ba45ef43a038ab42
Betty <zlhluoye@163.com>
2019-11-04 16:13:04 +0800

    reference

---
+++ Safeway/Controllers/NormalEntEvaTemplateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Mvc;
+using WalkingTec.Mvvm.Core.Extensions;
+using Safeway.ViewModel.NormalEntEvaTemplateListVMs;
+
+namespace Safeway.Controllers
+{
+    public class NormalEntEvaTemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new NormalEntEvaTemplateListVM();
+            
+            return View(vm.Index());
+        }
+    }
+}
