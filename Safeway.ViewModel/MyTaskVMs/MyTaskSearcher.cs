265fb73bd34c5cdeb041b2b9cb7d9e5828ffed0b
Focus <focusdada@gmail.com>
2019-12-02 17:30:28 +0800

    我的任务

---
+++ Safeway.ViewModel/MyTaskVMs/MyTaskSearcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WalkingTec.Mvvm.Core;
+using System.ComponentModel.DataAnnotations;
+using Safeway.Model.Common;
+
+namespace Safeway.ViewModel.MyTaskVMs
+{
+    public partial class MyTaskSearcher : BaseSearcher
+    {
+        [Display(Name = "项目名称")]
+        public String ProjectId { get; set; }
+        [Display(Name = "企业名称")]
+        public String EnterpriseId { get; set; }
+        [Display(Name = "开始时间")]
+        public DateTime? EvaluationStartDate { get; set; }
+        [Display(Name = "结束时间")]
+        public DateTime? EvaluationEndDate { get; set; }
+
+        [Display(Name = "状态")]
+        public EvaluationStatus? Status { get; set; }
+
+        [Display(Name = "属地")]
+        public string Street { get; set; }
+
+        [Display(Name = "行业")]
+        public string Industry { get; set; }
+
+        protected override void InitVM()
+        {
+        }
+    }
+}
---
+++ Safeway/Controllers/MyTaskController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Mvc;
+using WalkingTec.Mvvm.Core.Extensions;
+using Safeway.ViewModel.ProjectBasicInfoVMs;
+using System.Threading.Tasks;
+using Safeway.ViewModel.MyTaskVMs;
+using Safeway.ViewModel.CommonClass;
+
+namespace Safeway.Controllers
+{
+    [ActionDescription("我的任务")]
+    public class MyTaskController : BaseController
+    {
+        #region 搜索
+        [ActionDescription("搜索")]
+        public ActionResult Index()
+        {
+            var vm = CreateVM<MyTaskListVM>();
+            var commonvm = CreateVM<CommonVM>();
+            vm.AllEnterprise = commonvm.GetEnterprises();
+            vm.AllProject = commonvm.GetProjects();
+            return PartialView(vm);
+        }
+
+        [ActionDescription("搜索")]
+        [HttpPost]
+        public string Search(MyTaskListVM vm)
+        {
+            return vm.GetJson(false);
+        }
+
+        #endregion
+    }
+}
